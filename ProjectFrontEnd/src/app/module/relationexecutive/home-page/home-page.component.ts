c8c1f741d85bed81d277fb66a498011663b3ea50
abhilashthakre <abhilashthakre28@gmail.com>
2023-01-22 16:31:25 +0530

    charts in homepage

--- ProjectFrontEnd/src/app/module/relationexecutive/home-page/home-page.component.ts
+++ ProjectFrontEnd/src/app/module/relationexecutive/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Chart,registerables} from 'node_modules/chart.js'
+Chart.register(...registerables)
 
 @Component({
   selector: 'app-home-page',
@@ -7,4 +9,70 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
+  ngOnInit(){
+
+    this.renderChart()
+    this.renderChart1()
+  }
+
+  renderChart(){
+
+    new Chart("pieChart", {
+      type: 'doughnut',
+      data: {
+        labels: ['Enquiry', 'Rejected', 'Loan Registered', 'Pending', 'Sanctioned', 'NPA'],
+        datasets: [{
+          label: 'Applications',
+          data: [521, 59, 398, 102, 168, 36],
+          backgroundColor:[
+            'rgba(7, 118, 245,0.8)',
+            'rgba(245, 11, 7,0.8)',
+            'rgba(245, 213, 7,0.8)',
+            'rgba(245, 126, 7,0.8)',
+            'rgba(27, 245, 7,0.8)',
+            'rgba(245, 7, 209,0.8)'
+          ],
+          borderWidth: 1
+        }]
+        
+      },
+      // options: {
+      //   scales: {
+      //     y: {
+      //       beginAtZero: true
+      //     }
+      //   }
+      // }
+    });
+  }
+
+  renderChart1(){
+
+    new Chart("polarChart", {
+      type: 'polarArea',
+      data: {
+        labels: ['Secured MSME', 'Loan Against Gold', 'Overdraft', 'Refinance', 'Export Credit', 'Equipment Loan'],
+        datasets: [{
+          label: 'Amount in Rs(lakhs)',
+          data: [27.9, 25.2, 23.8, 18.5, 21.8, 17.5],
+          backgroundColor:[
+            'rgba(245, 11, 7,0.8)',
+            'rgba(7, 118, 245,0.8)',
+            'rgba(245, 213, 7,0.8)',
+            'rgba(27, 245, 7,0.8)',
+            'rgba(245, 7, 209,0.8)',
+            'rgba(245, 126, 7,0.8)',
+          ],
+          borderWidth: 1
+        }]
+      },
+      // options: {
+      //   scales: {
+      //     y: {
+      //       beginAtZero: true
+      //     }
+      //   }
+      // }
+    });
+  }
 }
