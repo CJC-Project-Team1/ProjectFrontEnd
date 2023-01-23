fed67b7488619829e3c8ffca2d27639b7c36fc51
SURUCHI1212 <surunautiyal@gmail.com>
2023-01-23 15:14:03 +0530

    ICON ROUTING

--- ProjectFrontEnd/src/app/module/relationexecutive/view-applicaion/view-applicaion.component.ts
+++ ProjectFrontEnd/src/app/module/relationexecutive/view-applicaion/view-applicaion.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Enquiry } from 'src/app/model/enquiry';
 import { EnquiryService } from 'src/app/shared/enquiry.service';
+import { NotifierService } from 'src/app/shared/notifier.service';
 
 @Component({
   selector: 'app-view-applicaion',
@@ -16,11 +17,17 @@ export class ViewApplicaionComponent {
   count: number = 0;
   tableItems: number = 5;
   tableSizes: any = [2, 4, 6, 8];
+   filteredEnqs:Enquiry[];
+  _filterText:string = '';
 
-  constructor(private es: EnquiryService, private route: Router) { }
+  constructor(private es: EnquiryService, private route: Router,private notify:NotifierService) { }
 
   ngOnInit() {
-    this.getData();
+    this.filteredEnqs=this.enq;
+   this.getData();
+   this.notify.info("ENQUIRY LIST","TABLE");
+   
+   
  
   }
 
@@ -31,6 +38,34 @@ export class ViewApplicaionComponent {
 
   }
 
+
+  get filterText()
+  {
+    return this._filterText;
+  }
+
+  set filterText(value:string)
+  {
+    this._filterText = value;
+    this.filteredEnqs = this.filterEnqByStatus(value);
+  }
+
+
+
+  filterEnqByStatus(filterTerm:string)
+  {
+    if(this.enq.length === 0 || this.filterText === '')
+    {
+      return this.enq;
+    }
+    else
+    {
+      return this.enq.filter((enquiry)=>{
+        return enquiry.enquiryStatus.toLowerCase() === filterTerm.toLowerCase();
+      });
+    }
+  }
+  
   onTableData(event:any)
   {
     this.page=event;
